c39e19f6bfef16b72b6ca419365b9498a16d1578
Artem Kuznetcov <artem.o.kuznecov@gmail.com>
2021-10-12 15:33:08 +0300

    Add bullet sources

--- Source/exactoSim/exactoSim.Build.cs
+++ Source/exactoSim/exactoSim.Build.cs
@@ -1,5 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
-
+using System;
+using System.IO;
 using UnrealBuildTool;
 
 public class exactoSim : ModuleRules
@@ -19,5 +20,34 @@ public class exactoSim : ModuleRules
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
 		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+		AddBullet();
+	}
+	private string ThirdPartyPath
+	{
+		get { return Path.GetFullPath(Path.Combine(ModuleDirectory, "../../../ThirdParty/")); }
+	}
+	protected void AddBullet()
+	{
+		// This is real basic, only for a single platform & config (Win64)
+		// If you build for more variants you'll have to do some more work here
+
+		bool bDebug = Target.Configuration == UnrealTargetConfiguration.Debug || Target.Configuration == UnrealTargetConfiguration.DebugGame;
+		bool bDevelopment = Target.Configuration == UnrealTargetConfiguration.Development;
+
+		string buildFolder = bDebug ? "Debug" :
+			bDevelopment ? "RelWithDebInfo" : "Release";
+		string buildSuffix = bDebug ? "_Debug" :
+			bDevelopment ? "_RelWithDebugInfo" : "";
+
+		// Library path
+		string librariesPath = Path.Combine(ThirdPartyPath, "lib", "bullet", buildFolder);
+		PublicAdditionalLibraries.Add(Path.Combine(librariesPath, "BulletCollision" + buildSuffix + ".lib"));
+		PublicAdditionalLibraries.Add(Path.Combine(librariesPath, "BulletDynamics" + buildSuffix + ".lib"));
+		PublicAdditionalLibraries.Add(Path.Combine(librariesPath, "LinearMath" + buildSuffix + ".lib"));
+
+		// Include path (I'm just using the source here since Bullet has mixed src & headers)
+		PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "bullet3", "src"));
+		PublicDefinitions.Add("WITH_BULLET_BINDING=1");
+
 	}
 }
