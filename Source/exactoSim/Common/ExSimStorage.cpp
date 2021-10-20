ac7617e2f6ba115107a7e322016e102bdaf5a7e5
Artem Kuznetcov <artem.o.kuznecov@gmail.com>
2021-10-20 10:09:50 +0300

    add input and storage for data based on bp

---
+++ Source/exactoSim/Common/ExSimStorage.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ExSimStorage.h"
+#include <string>
+
+// Sets default values
+AExSimStorage::AExSimStorage()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AExSimStorage::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AExSimStorage::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AExSimStorage::updateDebugLog(FText text)
+{
+	UE_LOG(LogTemp, Warning,TEXT("%s"),*text.ToString());
+}
+
---
+++ Source/exactoSim/UI/ExSimPlayer.cpp
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ExSimPlayer.h"
+
+
+// Sets default values
+AExSimPlayer::AExSimPlayer()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AExSimPlayer::activateFunction()
+{
+	//some action
+	UE_LOG(LogTemp, Warning, TEXT("Activate function!"));
+}
+
+void AExSimPlayer::sendDataToStorage()
+{
+	if (DataStorage)
+	{
+		DataStorage->updateDebugLog(FText::FromString("Hello"));
+	}
+}
+
+// Called when the game starts or when spawned
+void AExSimPlayer::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AExSimPlayer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AExSimPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	InputComponent->BindAction("activateFunction", IE_Pressed, this, &AExSimPlayer::activateFunction);
+}
+
