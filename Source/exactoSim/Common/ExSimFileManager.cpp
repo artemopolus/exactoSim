9a34f83e16984bfc3357c9cad0e2b8a932abed19
Artem Kuznetcov <artem.o.kuznecov@gmail.com>
2021-12-02 08:05:20 +0300

    add exacto world

--- Source/exactoSim/Common/ExSimFileManager.cpp
+++ Source/exactoSim/Common/ExSimFileManager.cpp
@@ -15,7 +15,52 @@ AExSimFileManager::AExSimFileManager()
 void AExSimFileManager::BeginPlay()
 {
 	Super::BeginPlay();
+
+	PathToContentFolder = FPaths::ProjectContentDir();
+	UE_LOG(LogTemp, Warning, TEXT("Content Folder : %s  "), *PathToContentFolder  );
+
+	PathToConfig = PathToContentFolder + TEXT("Options/gen_object_config.json");
+
+	UE_LOG(LogTemp, Warning, TEXT("Config file : %s  "), *PathToConfig  );
+
+	PathToFilesFolder = FPlatformProcess::UserDir();
+	PathToFilesFolder.Append("exactoSim");
+	UE_LOG(LogTemp, Warning, TEXT("Files path : %s  "), *PathToFilesFolder  );
+
+	PathToDataFolder = PathToFilesFolder + TEXT("Data/");
+
+	DataFileName = TEXT("Session_");
+
+	//создаем файл сессии
+
+
+
+	IPlatformFile& file_manager = FPlatformFileManager::Get().GetPlatformFile();
 	
+	if (!file_manager.DirectoryExists(*PathToFilesFolder))
+	{
+		if (!file_manager.CreateDirectory(*PathToFilesFolder))
+		{
+			UE_LOG(LogTemp, Error, TEXT("Can't create basic folder"));
+			return;
+		}
+	}
+	if (!file_manager.DirectoryExists(*PathToDataFolder))
+	{
+		if (!file_manager.CreateDirectory(*PathToDataFolder))
+		{
+			UE_LOG(LogTemp, Error, TEXT("Can't create basic folder"));
+			return;			
+		}
+	}
+	
+	FString path_to_session = PathToDataFolder + DataFileName + FDateTime::UtcNow().ToString(TEXT("%Y%m%d_%H%M%S")) + TEXT(".txt");
+
+	UE_LOG(LogTemp, Warning, TEXT("Path to session file : %s  "), *path_to_session  );
+
+	FString init_session_str = TEXT("Start session in ") + FDateTime::UtcNow().ToString();
+
+	FFileHelper::SaveStringToFile(init_session_str,*path_to_session, FFileHelper::EEncodingOptions::ForceUTF8,&IFileManager::Get(),FILEWRITE_None);
 }
 
 // Called every frame
--- Source/exactoSim/Common/ExSimFileManager.h
+++ Source/exactoSim/Common/ExSimFileManager.h
@@ -15,6 +15,13 @@ public:
 	// Sets default values for this actor's properties
 	AExSimFileManager();
 
+private:
+	FString PathToConfig;
+	FString PathToContentFolder;
+	FString PathToFilesFolder;
+	FString PathToDataFolder;
+	FString DataFileName;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
