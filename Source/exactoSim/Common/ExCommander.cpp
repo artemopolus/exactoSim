5945da9f2f761d9b9d1d540ee61dc63608e4f65f
Artem Kuznetcov <artem.o.kuznecov@gmail.com>
2022-04-26 11:03:10 +0300

    rm garbage

--- Source/exactoSim/Common/ExCommander.cpp
+++ Source/exactoSim/Common/ExCommander.cpp
@@ -1,78 +1,7 @@
 #include "ExCommander.h"
 
 
-void ExStringPack::setActive(FExConstraintParams* target, EConstraintParamNames type, FString vec)
-{
-	Target = target;
-	Type = type;
-	Cur = vec;
-}
 
-bool ExStringPack::revert()
-{
-	return ExConvert::updateParams(Target, Type, Old);
-}
-
-bool ExStringPack::update()
-{
-	ExConvert::getParams(Target, Type, &Old);
-	return ExConvert::updateParams(Target, Type, Cur);
-}
-
-
-void ExVectorPack::setActive(FExConstraintParams* target, EConstraintParamNames type, FVector vec)
-{
-	Target = target;
-	Type = type;
-	Cur = vec;
-}
-
-bool ExVectorPack::update()
-{
-	ExConvert::getParams(Target, Type, &Old);
-	return ExConvert::updateParams(Target, Type, Cur);
-}
-
-bool ExVectorPack::revert()
-{
-	return ExConvert::updateParams(Target, Type, Old);
-}
-
-void ExConstraintPack::setActive(FExConstraintParams* target, EConstraintParamNames type, BulletHelpers::Constr vec)
-{
-	Target = target;
-	Type = type;
-	Cur = vec;
-}
-
-bool ExConstraintPack::update()
-{
-	ExConvert::getParams(Target, Type, &Old);
-	return ExConvert::updateParams(Target, Type, Cur);
-}
-
-bool ExConstraintPack::revert()
-{
-	return ExConvert::updateParams(Target, Type, Old);
-}
-
-void ExFloatPack::setActive(FExConstraintParams* target, EConstraintParamNames type, float vec)
-{
-	Target = target;
-	Type = type;
-	Cur = vec;
-}
-
-bool ExFloatPack::update()
-{
-	ExConvert::getParams(Target, Type, &Old);
-	return ExConvert::updateParams(Target, Type, Cur);
-}
-
-bool ExFloatPack::revert()
-{
-	return ExConvert::updateParams(Target, Type, Old);
-}
 
 void ExCommander::updateConstraint(EConstraintParamNames type, FVector vec)
 {
