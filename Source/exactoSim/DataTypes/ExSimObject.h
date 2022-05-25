1cccaadb5439c28139e7114df1f55956f7688d89
Artem Kuznetcov <artem.o.kuznecov@gmail.com>
2022-05-25 11:34:09 +0300

    add parent for component complex constraint

---
+++ Source/exactoSim/DataTypes/ExSimObject.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "ExSimPhyzHelpers.h"
+
+class EXACTOSIM_API ExSimObject
+{
+	EnExParamTypes ObjType = EnExParamTypes::CONSTRAINT;
+public:
+	virtual ~ExSimObject() = default;
+	EnExParamTypes getObjType() const {return  ObjType;}
+	void setObjType(EnExParamTypes type){ObjType = type;}
+	bool isConstraint() const{return ObjType == EnExParamTypes::CONSTRAINT;}
+	bool isComponent() const{return ObjType == EnExParamTypes::COMPONENT;}
+	bool isComplex() const{return ObjType == EnExParamTypes::COMPLEX;}
+	virtual void update() = 0;
+};
