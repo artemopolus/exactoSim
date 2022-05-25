4815d3fddcc08371745b4fba05e8c4d499360990
Artem Kuznetcov <artem.o.kuznecov@gmail.com>
2022-05-25 11:23:39 +0300

    refactor complex params

--- Source/exactoSim/DataTypes/ExComplexParams.cpp
+++ Source/exactoSim/DataTypes/ExComplexParams.cpp
@@ -2,6 +2,72 @@
 
 #include "exactoSim/Utils/ExConvert.h"
 
+bool FExComplexParams::update(EnExComplexParamNames type, FString val)
+{
+	if (type == EnExComplexParamNames::C_NAME)
+		Name = val;
+	else if (type == EnExComplexParamNames::C_BASIS_NAME)
+		BasisName = val;
+	else return false;
+	return true;
+}
+
+bool FExComplexParams::update(EnExComplexParamNames type, FVector val)
+{
+	return false;
+}
+
+bool FExComplexParams::update(EnExComplexParamNames type, FRotator val)
+{
+	return false;
+}
+
+bool FExComplexParams::update(EnExComplexParamNames type, float val)
+{
+	return false;
+}
+
+bool FExComplexParams::update(EnExComplexParamNames type, int32 val)
+{
+	return false;
+}
+
+bool FExComplexParams::copy(EnExComplexParamNames type, FString* trg)
+{
+	if (type == EnExComplexParamNames::C_NAME)
+		*trg = Name;
+	else if (type == EnExComplexParamNames::C_BASIS_NAME)
+		*trg = BasisName;
+	else return false;
+	return true;
+}
+
+bool FExComplexParams::copy(EnExComplexParamNames type, FVector* trg)
+{
+	return false;
+}
+
+bool FExComplexParams::copy(EnExComplexParamNames type, FRotator* trg)
+{
+	return false;
+}
+
+bool FExComplexParams::copy(EnExComplexParamNames type, float* trg)
+{
+	return false;
+}
+
+bool FExComplexParams::copy(EnExComplexParamNames type, int32* trg)
+{
+	return false;
+}
+
+void ExComplexDict::getInitValues(TMap<EnExComplexParamNames, TArray<FString>>* trg, FExComplexParams* params)
+{
+	trg->FindOrAdd(EnExComplexParamNames::C_NAME) = ExConvert::getArrayString(params->Name); 
+	trg->FindOrAdd(EnExComplexParamNames::C_BASIS_NAME) = ExConvert::getArrayString(params->BasisName); 
+}
+
 void ExComplexDict::updateValues(TMap<EnExComplexParamNames, FString>* trg, FExComplexParams* params)
 {
 	trg->FindOrAdd(EnExComplexParamNames::C_NAME) = params->Name; 
@@ -35,4 +101,14 @@ void ExComplexDict::fromNameValuePairsToParams(TMap<FString, FString>* src, FExC
 		const auto str = src->FindRef(name.Value);
 		ExConvert::updateParams(trg, name.Key, str);
 	}
+}
+
+void ExComplexDict::getEditTypeCreate(TMap<EnExComplexParamNames, EnExParamEdit>* trg)
+{
+	trg->FindOrAdd(EnExComplexParamNames::C_NAME) = EnExParamEdit::EDITABLE; 
+	trg->FindOrAdd(EnExComplexParamNames::C_BASIS_NAME) = EnExParamEdit::EDITABLE; 
+}
+
+void ExComplexDict::getEditTypeUpdate(TMap<EnExComplexParamNames, EnExParamEdit>* trg)
+{
 }
--- Source/exactoSim/DataTypes/ExComplexParams.h
+++ Source/exactoSim/DataTypes/ExComplexParams.h
@@ -3,19 +3,6 @@
 
 #include "ExComplexParams.generated.h"
 
-USTRUCT()
-struct EXACTOSIM_API FExComplexParams : public FExCommonParams
-{
-	GENERATED_BODY()
-	FString Name;
-	FString BasisName;
-	TArray<FString> ComponentNames;
-	FExComplexParams()
-	{
-		setType(EnExParamTypes::COMPLEX);
-	}
-	
-};
 enum class EnExComplexParamNames : int
 {
 	AA_VECTOR_START = 0,
@@ -28,13 +15,40 @@ enum class EnExComplexParamNames : int
 	ZZ_OPT_END,
 };
 
+USTRUCT()
+struct EXACTOSIM_API FExComplexParams : public FExCommonParams
+{
+	GENERATED_BODY()
+	FString Name;
+	FString BasisName;
+	TArray<FString> ComponentNames;
+	FExComplexParams()
+	{
+		setType(EnExParamTypes::COMPLEX);
+	}
+	bool update(EnExComplexParamNames type, FString val);
+	bool update(EnExComplexParamNames type, FVector val);
+	bool update(EnExComplexParamNames type, FRotator val);
+	bool update(EnExComplexParamNames type, float val);
+	bool update(EnExComplexParamNames type, int32 val);
+	bool copy(EnExComplexParamNames type, FString * trg);
+	bool copy(EnExComplexParamNames type, FVector * trg);
+	bool copy(EnExComplexParamNames type, FRotator * trg);
+	bool copy(EnExComplexParamNames type, float * trg);
+	bool copy(EnExComplexParamNames type, int32 * trg);
+};
+
+
 class EXACTOSIM_API ExComplexDict
 {
 public:
+	static void getInitValues(TMap<EnExComplexParamNames, TArray<FString>>* trg, FExComplexParams* params);
 	static void updateValues(TMap<EnExComplexParamNames, FString>* trg, FExComplexParams* params);
 	static void getDefaultNames(TMap<EnExComplexParamNames, FString>* trg);
 	static void getNameValuePairs(TMap<EnExComplexParamNames, FString>* names,
 	                              TMap<EnExComplexParamNames, FString>* values, TMap<FString, FString>* trg);
 	static void fromNameValuePairsToParams(TMap<FString, FString> * src, FExComplexParams * trg);
+	static void getEditTypeCreate(TMap<EnExComplexParamNames, EnExParamEdit>* trg);
+	static void getEditTypeUpdate(TMap<EnExComplexParamNames, EnExParamEdit>* trg);
 };
 
